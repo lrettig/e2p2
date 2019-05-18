9b3c02ab15cbe2612a972acb6a782fd71cb9f84d
Lane Rettig <lanerettig@gmail.com>
2019-05-18 15:36:43 -0400

    Initial page layout with shards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react'
+import './App.css'
+
+const shardCount = 4
 
 function App() {
+  let shards = []
+  for (let i = 0; i < shardCount; i++) {
+    const row = Math.floor(i/2)
+    if (i % 2 === 0)
+      shards[row] = []
+    shards[row].push(i)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          E2P2 Simulator
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+        {
+          shards.map((arr, row) =>
+            <div key={row}>{
+              arr.map(n => <Shard id={n} key={n}/>)
+            }</div>
+          )
+        }
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+class Shard extends Component {
+  render() {
+    const {id} = this.props
+    return (
+      <div>
+        <h3>Shard {id}</h3>
+      </div>
+    )
+  }
+}
+
+export default App
